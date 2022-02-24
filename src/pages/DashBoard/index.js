be513e632f7e27237bddc69985d06790781a90b4
Diego Ramos <diego_ramos93@outlook.com>
2022-02-23 23:37:28 -0300

    criacao pagina registro de chamados

--- src/pages/DashBoard/index.js
+++ src/pages/DashBoard/index.js
@@ -3,11 +3,11 @@ import {useState} from 'react';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import {FiMessageSquare, FiPlus} from 'react-icons/fi';
+import {FiMessageSquare, FiPlus, FiSearch, FiEdit2} from 'react-icons/fi';
 import {Link} from 'react-router-dom';
 
 export default function DashBoard(){
-    const [chamados, setChamados] = useState([]);
+    const [chamados, setChamados] = useState([1]);
     return(
         <div>
             <Header/>
@@ -32,6 +32,41 @@ export default function DashBoard(){
                     <FiPlus size={25} color="#FFF" />
                     Novo Chamado
                     </Link>
+
+                    <table>
+                        {/*cabeçalho*/}
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assuntos</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        {/*cabeçalho até aqui*/}
+
+                        {/*Corpo*/}
+                        <tbody>
+                            <tr>
+                                <td data-label="Cliente">Sujeito</td>
+                                <td data-label="Assunto">Suporte</td>
+                                <td data-label="Status">
+                                    <span className="badge" style={{backgroundColor: '#5CB85C'}}>Em aberto</span>
+                                </td>
+                                <td data-label="Cadastrado">20/06/2021</td>
+                                <td data-label="#">
+                                    <button className="action" style={{backgroundColor: '#3583F6'}}>
+                                        <FiSearch color="#FFF" size={17} />
+                                    </button>
+                                    <button className="action" style={{backgroundColor: '#F6A935'}}>
+                                        <FiEdit2 color="#FFF" size={17} />
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                        {/*Fim do corpo*/}
+                    </table>
                     </>
                 )}
 
